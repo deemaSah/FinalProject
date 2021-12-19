e6bfbf73607a9be8f8f522f9d5bed4e76db124c1
deemaSah <deemasahyoni@gmail.com>
2021-12-19 18:12:48 +0200

    version 1.1

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -15,4 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void getGalleryActivity(View view) {
+        Intent intent = new Intent(this,Gallery.class);
+        startActivity(intent);
+    }
 }
