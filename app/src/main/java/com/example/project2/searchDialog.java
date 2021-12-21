9d414a02569bf5788f3b04916d90956969fee636
Batool_Dahnon <batooldahnon4@gmail.com>
2021-12-21 19:42:04 +0200

    version2.0

---
+++ app/src/main/java/com/example/project2/searchDialog.java
@@ -0,0 +1,43 @@
+package com.example.project2;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class searchDialog extends AppCompatDialogFragment {
+    TextView adultscounter;
+    Button adultsplus;
+    int counterads = 0;
+    @Override
+
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_search_dialog,null);
+        builder.setView(view);
+        adultscounter =view.findViewById(R.id.adultscounter);
+        adultsplus=view.findViewById(R.id.adultsplus);
+        builder.setTitle("Search");
+        builder.setPositiveButton("search", new DialogInterface.OnClickListener() {
+            @Override
+            //close the diologe
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        return builder.create();
+    }
+    public void  setAdultscounter (View view){
+        counterads++;
+        adultscounter.setText(Integer.toString(counterads));
+    }
+}
